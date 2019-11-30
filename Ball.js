98d85bed075e5d59c0c840145a4780960d79ab8f
Sourov Sarkar <42178766+Leoperon@users.noreply.github.com>
2019-12-01 00:52:03 +0530

    Update Ball file

--- Ball.js
+++ Ball.js
@@ -2,18 +2,16 @@ function Ball(x, y, radius) {
 
 	this.x = x;
 	this.y = y;
-
-	this.yVelocity = 0;
+    this.yVelocity = 0;
 	this.speed = 1;
 	this.onGround = true;
-
-	this.radius = radius; 
+    this.radius = radius; 
+}
 
 Ball.prototype.update = function(platform) {
 
-	var bottom = this.y + this.radius; 
-	var nextBottom = bottom + this.yVelocity; 
-
+  var bottom = this.y + this.radius; 
+  var nextBottom = bottom + this.yVelocity; 
   if (bottom <= platform && nextBottom >= platform) { 
 
 		this.yVelocity = 0; 
@@ -24,20 +22,16 @@ Ball.prototype.update = function(platform) {
 		this.yVelocity += this.speed; 
 		this.onGround = false;
   }
-
-	
-	this.y += this.yVelocity;
+  this.y += this.yVelocity;
 };
-
 Ball.prototype.jump = function() {
 
 	this.yVelocity = -(this.radius * 0.7); 
 };
-
 Ball.prototype.draw = function() {
 
-  fill('#999999');
-	stroke(255);
-	strokeWeight(2);
+  fill('#ff3333');
+  stroke(255);
+  strokeWeight(2);
   ellipse(this.x, this.y, this.radius * 2);
 };
